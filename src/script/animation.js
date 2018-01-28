4ca65ffb6229d6a2be372c3d53c81c13c2a75b57
pekko1215 <pekko1215@yahoo.co.jp>
2018-01-28 22:14:15 +0900

    prototype

--- src/script/animation.js
+++ src/script/animation.js
@@ -5,27 +5,153 @@ window.addEventListener('load', () => {
         var min = 160;
         var max = 400;
         document.getElementById('list-button').addEventListener('click', () => {
-			if(timer){return}
-			if(!resized){
-				timer = setInterval(()=>{
-					if(window.outerHeight < max){
-						window.resizeBy(0,10);
-					}else{
-						clearInterval(timer);
-						timer = null;
-						resized = true
-					}
-				},10)
-			}else{
-				timer = setInterval(()=>{
-					if(window.outerHeight > min){
-						window.resizeBy(0,-10);
-					}else{
-						clearInterval(timer);
-						timer = null;
-						resized = false
-					}
-				},10)
+            if (timer) { return }
+            if (!resized) {
+                timer = setInterval(() => {
+                    if (window.outerHeight < max) {
+                        window.resizeBy(0, 10);
+                    } else {
+                        clearInterval(timer);
+                        timer = null;
+                        resized = true
+                    }
+                }, 10)
+            } else {
+                timer = setInterval(() => {
+                    if (window.outerHeight > min) {
+                        window.resizeBy(0, -10);
+                    } else {
+                        clearInterval(timer);
+                        timer = null;
+                        resized = false
+                    }
+                }, 10)
+            }
+        })
+    })();
+    (function() {
+        var $playButton = document.getElementById('play-button');
+        var $pauseButton = document.getElementById('pause-button');
+        var $playButtonWrapper = document.getElementById('play-button-wrapper');
+        var $pauseButtonWrapper = document.getElementById('pause-button-wrapper');
+        var isStop = false;
+        $pauseButtonWrapper.addEventListener('click', () => {
+            if (isStop) { return };
+            if (MusicQueue.pause()) {
+                $pauseButton.classList.toggle('active')
+                $pauseButtonWrapper.classList.toggle('active');
+                $playButton.classList.toggle('active')
+                $playButtonWrapper.classList.toggle('active');
+                isStop = true;
+            }
+        })
+        $playButtonWrapper.addEventListener('click', () => {
+            if (!isStop) { return };
+            if (MusicQueue.resume()) {
+                $pauseButton.classList.toggle('active')
+                $pauseButtonWrapper.classList.toggle('active');
+                $playButton.classList.toggle('active')
+                $playButtonWrapper.classList.toggle('active');
+                isStop = false;
+            }
+        })
+    })();
+    (function() {
+        var timer = null;
+        MusicQueue.on('play', (music) => {
+            if (timer) { clearInterval(timer) }
+            var length = music.source.buffer.duration;
+            var lengthtext = `${Math.floor(length/60)}:${('00'+Math.floor(length%60)).slice(-2)}`
+            var starttime = music.source.context.currentTime;
+            document.getElementById('title').innerText = music.name
+            document.getElementById('time_under').innerText = lengthtext;
+
+            function rewriter() {
+                var currentTime = music.source.context.currentTime - starttime;
+                if (currentTime > length) {
+                    currentTime = length
+                    clearInterval(timer);
+                    timer = null;
+                }
+                var currenttext = `${Math.floor(currentTime/60)}:${('00'+Math.floor(currentTime%60)).slice(-2)}`
+                document.getElementById('time_over').innerText = currenttext;
+            }
+            timer = setInterval(rewriter, 500);
+            rewriter();
+        })
+    })();
+    (function() {
+        var $playlistContainer = document.getElementById('playlist-container');
+        $playlistContainer.ondragover = function() {
+			$playlistContainer.classList.add('drag')
+			return false;
+        }
+        $playlistContainer.ondragleave = $playlistContainer.ondragend = function() {
+			$playlistContainer.classList.remove('drag')
+            return false;
+        };
+        $playlistContainer.ondrop = function(e) {
+            e.preventDefault(); // イベントの伝搬を止めて、アプリケーションのHTMLとファイルが差し替わらないようにする
+            $playlistContainer.classList.remove('drag')
+            e.dataTransfer.types.forEach((type)=>{
+				switch(type){
+					case 'text/plain':
+						break;
+					case 'Files':
+						for (var i = 0;i < e.dataTransfer.files.length;i++){
+							var file = e.dataTransfer.files[i];
+							if(fs.statSync(file.path).isDirectory()){
+
+							}else{
+								var music = new Music();
+								music.loadByFile(file.path).then(()=>{
+									MusicQueue.enqueue(music)
+								})
+							}
+						}
+						break;
+				}
+            })
+            return false;
+        };
+    })();
+    function reloadListDom(){
+		var $tbody = document.getElementById('playlist_tbody');
+		while ($tbody.firstChild)$tbody.removeChild($tbody.firstChild);
+		MusicQueue.queue.forEach(music=>{
+			var $tr = document.createElement('tr');
+			var $nametd = document.createElement('td');
+			var $pathtd = document.createElement('td');
+			var $timetd = document.createElement('td');
+			$nametd.innerText = music.name;
+			$pathtd.innerText = music.path;
+			var time = music.buffer.duration;
+			$timetd.innerText = `${Math.floor(time/60)}:${('00'+Math.floor(time%60)).slice(-2)}`
+			$tr.appendChild($nametd)
+			$tr.appendChild($pathtd)
+			$tr.appendChild($timetd);
+			$tbody.appendChild($tr)
+		})
+    }
+    MusicQueue.on('enqueue',reloadListDom);
+    MusicQueue.on('dequeue',reloadListDom);
+    (function(){
+		var $recycleButton = document.getElementById('recycle-button')
+		var $repeatButton = document.getElementById('repeat-button')
+		$recycleButton.addEventListener('click',()=>{
+			window.recycle = !window.recycle;
+			$recycleButton.classList.toggle('active');
+			if(window.repeat&&window.recycle){
+				window.repeat = false;
+				$repeatButton.classList.remove('active')
+			}
+		})
+		$repeatButton.addEventListener('click',()=>{
+			window.repeat = !window.repeat;
+			$repeatButton.classList.toggle('active');
+			if(window.repeat&&window.recycle){
+				window.recycle = false;
+				$recycleButton.classList.remove('active')
 			}
 		})
     })()
--- src/script/toivoa.js
+++ src/script/toivoa.js
@@ -2,6 +2,10 @@ const { remote } = require('electron')
 const { Menu } = remote;
 const { MenuItem } = remote;
 var contextMenu = new Menu();
+const path = require('path');
+
+const fs = require('fs')
+
 contextMenu.append(new MenuItem({
     label: '追加',
     click: function() {
@@ -14,4 +18,120 @@ window.addEventListener('contextmenu', function(e) {
     contextMenu.popup(remote.getCurrentWindow());
 }, false);
 
-window.addEventListener('load', () => {})
+var MusicQueue = {
+    events: {},
+    queue: [],
+    current: null,
+    enqueue: function(music) {
+        this.queue.push(music);
+        this.emit('enqueue', music);
+        return this.queue.length;
+    },
+    dequeue: function() {
+        var music = this.queue.shift();
+        this.emit('dequeue', music);
+        return music;
+    },
+    remove: function(idx) {
+        var music = this.queue.splice(idx, 1);
+        this.emit('remove', music);
+        return music;
+    },
+    emit: function(ev, param) {
+        if (this.events[ev]) {
+            this.events[ev].forEach((fn, i) => {
+                fn(param);
+                if (fn.once) { delete this.events[ev][i] }
+            })
+        }
+    },
+    on: function(ev, fn) {
+        this.events[ev] ? this.events[ev].push(fn) : this.events[ev] = [fn];
+    },
+    once: function(ev, fn) {
+        fn.once = true;
+        this.on(ev, fn)
+    },
+    play: function() {
+        if (this.current) {
+			this.current.stop();
+        }
+        var music = this.loopFlag?this.currentMusic:this.dequeue();
+        if (!music) { return false }
+		this.current = context.createBufferSource();
+		this.current.buffer = music.buffer;
+		this.current.connect(this.node);
+		this.current.start(0);
+		music.source = this.current
+		this.currentMusic = music
+		this.current.onended = ()=>{
+			this.emit('end',music);
+		}
+		this.emit('play',music);
+		return this.current;
+    },
+    pause: function() {
+        if (!this.current) {
+			return false;
+        }
+        if(this.stoped){return false}
+		this.stoped = true;
+		context.suspend()
+		return true;
+    },
+    resume:function(){
+		if(!this.current){return false}
+		if(!this.stoped){return false}
+		this.stoped = false;
+		context.resume()
+		return true;
+    },
+    init: function(node) {
+        this.node = node;
+    },
+    stoped:false,
+    loopFlag: false,
+    pausetime:-1
+};
+
+MusicQueue.on('end',(music)=>{
+	if(window.recycle){
+		MusicQueue.enqueue(music);
+	}
+	if(window.repeat){
+		MusicQueue.loopFlag = true;
+	}
+	MusicQueue.play();
+})
+
+var Music = function(_option) {};
+
+Music.prototype.loadByFile = function(dir) {
+	dir = dir.replace(/\\/g,"/");
+	return new Promise((resolve,reject)=>{
+		fs.readFile(dir, (err, data) => {
+			window.context.decodeAudioData(Music.toArrayBuffer(data), (buffer) => {
+			    if(err){reject(err);return}
+			    this.buffer = buffer;
+			    this.path = dir;
+			    var arr = dir.split('/');
+			    this.name = arr[arr.length-1]
+			    resolve();
+		    })
+	    })
+	})
+}
+
+
+Music.toArrayBuffer = function(buf) {
+    return buf.buffer.slice(buf.byteOffset, buf.byteOffset + buf.byteLength);
+}
+
+
+window.addEventListener('load', () => {
+    window.context = new AudioContext;
+    var gainNode = context.createGain();
+    gainNode.gain.value = 0.5
+    gainNode.connect(context.destination);
+    MusicQueue.init(gainNode);
+})
